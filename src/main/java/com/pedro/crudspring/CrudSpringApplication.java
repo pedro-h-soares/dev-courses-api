e1bfb4e873e25eb8c7eaef321561f32216be081e
pedro.soares <pedro.soares@etaure.com.br>
2022-11-22 16:28:14 -0300

    feature: working list courses

--- src/main/java/com/pedro/crudspring/CrudSpringApplication.java
+++ src/main/java/com/pedro/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.pedro.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.pedro.crudspring.model.Course;
+import com.pedro.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
